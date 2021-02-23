22f257c4c1127231e9d9d895b6c6b4a8e9115c2e
masoudpo <79519644+masoudpo@users.noreply.github.com>
2021-02-23 18:40:09 +0330

    Update bot.php

--- bot/bot.php
+++ bot/bot.php
@@ -15,3 +15,34 @@ $url = "https://api.telegram.org/bot".$token.'/sendMessage?chat_id='.$result['me
 file_get_contents($url);
 
 ?>
+<?php
+https://api.tlegram.org/bot1678126803:AAHOW3SZQQ7uNothDbJpkprCymbkBHwD_yU/setwebhook
+
+
+$update = file_get_contents("php://input");
+$update_array = json_decode($update , true)
+if (insset($update_array["message"])){
+$text = $update_array["message"]["text"]
+$chat_id = $update_array["message"]["chat"]["id"]
+
+}
+//---------------------------------
+$reply = "مسعود جون" . $GLOBALS['text'];
+$url ="https://api.tlegram.org/bot"."1678126803:AAHOW3SZQQ7uNothDbJpkprCymbkBHwD_yU" . "/sendmesseage";
+$post_params = ['chat_id'=>$GLOBALS['chat_id'], 'text' => $reply];
+send_reply($url, $post_params);
+//---------------------------
+function send_reply ($url,$post_params)
+{ $cu = curl_init();
+curl_setopt(cu, curlopt_url, $url);
+curl_setopt(cu, curlopt_postfields, $post_params);
+curl_setopt(cu, curlopt_returntransfer, true);
+$result = curl_exec($cu)
+curl_close($cu)
+return $result;
+
+
+
+}
+
+
